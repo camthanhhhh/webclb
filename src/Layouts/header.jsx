34805e8bec31639d3d34eb121349b43047d30dba
camthanhhhh <camthanhdao21@gmail.com>
2024-07-26 15:26:50 +0700

    hello bà zà

--- src/Layouts/header.jsx
+++ src/Layouts/header.jsx
@@ -1,11 +1,29 @@
 
-import { Container, Typography } from "@mui/material";
+import { Container } from "@mui/material";
+import './CSS/header.css';
 
 function Header() {
   return (
-    <Container>
-      Header
-    </Container>
+    <Container class="header">
+      <img src="logo.png" alt="Logo USAC" className="logo" />
+      <nav>
+          <ul>
+              {/* <li><a href="/">Home</a></li> */}
+              <li><a href="/news">News</a></li>
+              <li><a href="/facts">Facts</a></li>
+              <li><a href="/calendars">Calendars</a></li>
+              <li><a href="/academic">Academic <br/> Articles</a></li>
+              <li><a href="/science">Science</a></li>
+              <li><a href="/activities">Activities</a></li>
+              <li><a href="/aboutus">About us</a></li>
+          </ul>
+      </nav>
+
+      <div className="search-container">
+        <input type="text" placeholder="Type here for searching..." className="search-input" />
+        <button type="submit" className="search-button">🔍</button>
+      </div>
+  </Container>
   );
 }
 
--- src/Pages/home-page/home_page.jsx
+++ src/Pages/home-page/home_page.jsx
@@ -1,6 +1,7 @@
 import { Container, Typography } from "@mui/material";
 // import { useTheme } from "@emotion/react";
 // import { useEffect, useState } from "react";
+import './home_page.css'
 
 
 function HomePage() {
@@ -8,8 +9,9 @@ function HomePage() {
   return (
     <Container>
       <Typography fontSize={20} >
-        Truyện Hot
+        <img src="/Home.png" size="width: 100%" alt="Home" className="home-image" />
       </Typography>
+      
       {/* <NovelGrid novels={hot_novels}></NovelGrid>
       <NovelTable novel_data={other_sources_hot_novel}></NovelTable> */}
     </Container>
